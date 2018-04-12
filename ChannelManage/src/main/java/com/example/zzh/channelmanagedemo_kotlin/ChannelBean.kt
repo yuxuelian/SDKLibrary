6655c4b08a15d17f6c22346ff417bb4eff5bdf03
王开波 <568966289@qq.com>
2018-04-12 17:21:49 +0800

    ChannelManage模块改成java代码

--- ChannelManage/src/main/java/com/example/zzh/channelmanagedemo_kotlin/ChannelBean.kt
+++
@@ -1,8 +0,0 @@
-package com.example.zzh.channelmanagedemo_kotlin
-
-/**
- * Created by zhangzhihao on 2018/3/6.
- */
-class ChannelBean(var name:String ,var spanSize:Int ,var layoutId: Int,var isRecommend: Boolean){
-}
-
